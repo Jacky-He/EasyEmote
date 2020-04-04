747a9ab2198edb37e4c417f04d36d1222182ebce
Jacky He <jacky.he.juncheng@gmail.com>
2020-04-04 11:28:31 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.browserAction.onClicked.addListener(function(tab)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs [0];
+        chrome.tabs.sendMessage(activeTab.id, {"message" : "clicked_browser_action"});
+    })
+});
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message == "open_new_tab")
+        {
+            chrome.tabs.create({"url" : request.url});
+        }
+    }
+);
---
+++ content.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message == "clicked_browser_action") {
+            var firstHref = $("a[href^='http']").eq(0).attr("href");
+            alert(firstHref);
+            chrome.runtime.sendMessage({"message":"open_new_tab", "url":firstHref});
+        }
+    }
+)
