d32b0e37eb3976ef44987770347c07705d2a7f6d
Jacky He <jacky.he.juncheng@gmail.com>
2020-11-23 16:33:43 -0500

    Initial Commit

---
+++ EasyEmoteMacOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  EasyEmoteMacOS
+//
+//  Created by Jacky He on 2020-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
